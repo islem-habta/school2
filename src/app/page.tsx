c1f561cb80c9fd351fc6d9f2a58d28f51c01412c
islem <islemhabta@gmail.com>
2025-08-14 22:45:00 +0200

    Updated: Removed home page redirect, made admin dashboard the main page with full navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,74 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+'use client';
 
-export default Homepage
+import Announcements from "@/components/Announcements";
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+const SchoolDashboard = () => {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT SIDEBAR */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 bg-white border-r">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 mb-6"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block font-bold text-lg">School Dashboard</span>
+        </Link>
+        <Menu />
+      </div>
+      
+      {/* RIGHT CONTENT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        
+        {/* MAIN DASHBOARD CONTENT */}
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+          {/* LEFT */}
+          <div className="w-full lg:w-2/3 flex flex-col gap-8">
+            {/* USER CARDS */}
+            <div className="flex gap-4 justify-between flex-wrap">
+              <UserCard type="student" />
+              <UserCard type="teacher" />
+              <UserCard type="parent" />
+              <UserCard type="staff" />
+            </div>
+            {/* MIDDLE CHARTS */}
+            <div className="flex gap-4 flex-col lg:flex-row">
+              {/* COUNT CHART */}
+              <div className="w-full lg:w-1/3 h-[450px]">
+                <CountChart />
+              </div>
+              {/* ATTENDANCE CHART */}
+              <div className="w-full lg:w-2/3 h-[450px]">
+                <AttendanceChart />
+              </div>
+            </div>
+            {/* BOTTOM CHART */}
+            <div className="w-full h-[500px]">
+              <FinanceChart />
+            </div>
+          </div>
+          {/* RIGHT */}
+          <div className="w-full lg:w-1/3 flex flex-col gap-8">
+            <div className="flex justify-between mb-4">
+              
+            </div>
+            <EventCalendar />
+            <Announcements />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SchoolDashboard;
